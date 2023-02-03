f3992844cb548703d329c7ca05e023c5fa261d5f
Francisco Paliouras <franciscopaliouras@gmail.com>
2023-02-03 18:14:44 -0500

    adding contact form

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
 import tailwind from "@astrojs/tailwind";
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), vue()],
   output: "server",
-  adapter: netlify(),
+  adapter: netlify()
 });
---
+++ src/components/ContactForm.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { ref } from 'vue';
+
+const name = ref('');
+const email = ref('');
+const message = ref('');
+
+function submitForm() {
+  // Add code to submit the form
+  alert('joke')
+}
+</script>
+
+<template>
+    <form name="Contact-Portafolio"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field" class="bg-[#081710] p-6 rounded-lg shadow-md">
+    <input type="hidden" name="form-name" value="Contact-Portafolio" />
+        <div class="mb-4">
+          <label class="block text-[#3BA576] font-medium mb-2" for="name">
+            Name
+          </label>
+          <input
+            class="w-full border text-[#3BA576] bg-[#081710] p-2 rounded-lg"
+            id="name"
+            type="text"
+            v-model="name"
+          />
+        </div>
+        <div class="mb-4">
+          <label class="block text-[#3BA576] font-medium mb-2" for="email">
+            Email
+          </label>
+          <input
+            class="w-full border text-[#3BA576] bg-[#081710] p-2 rounded-lg"
+            id="email"
+            type="email"
+            v-model="email"
+          />
+        </div>
+        <div class="mb-4">
+          <label class="block text-[#3BA576] font-medium mb-2" for="message">
+            Message
+          </label>
+          <textarea
+            class="w-full border text-[#3BA576] bg-[#081710] p-2 rounded-lg"
+            id="message"
+            rows="5"
+            v-model="message"
+          ></textarea>
+        </div>
+        <div class="flex items-center justify-between">
+          <button
+            class="border-2 border-[#3BA576] text-[#3BA576] py-2 px-4 rounded-lg hover:bg-[#3BA576] hover:text-black"
+            @click="submitForm"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+</template>
