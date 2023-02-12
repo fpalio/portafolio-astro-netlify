7a44f74ebe02489f4afc1459e8d90488629c5278
Francisco Paliouras <franciscopaliouras@gmail.com>
2023-02-12 13:03:47 -0500

    added forms spree

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -4,25 +4,20 @@ import { ref } from 'vue';
 const name = ref('');
 const email = ref('');
 const message = ref('');
-
-function submitForm() {
-  // Add code to submit the form
-  alert('joke')
-}
 </script>
 
 <template>
     <form name="Contact-Portafolio"
-    netlify
-    method="post"
-    data-netlify="true"
-    data-netlify-honeypot="bot-field" class="bg-[#081710] p-6 rounded-lg shadow-md">
+    action="https://formspree.io/f/xeqwapgo"
+    method="POST"
+    class="bg-[#081710] p-6 rounded-lg shadow-md">
     <input type="hidden" name="form-name" value="Contact-Portafolio" />
         <div class="mb-4">
           <label class="block text-[#3BA576] font-medium mb-2" for="name">
             Name
           </label>
           <input
+            name="name"
             class="w-full border text-[#3BA576] bg-[#081710] p-2 rounded-lg"
             id="name"
             type="text"
@@ -34,6 +29,7 @@ function submitForm() {
             Email
           </label>
           <input
+            name="email"
             class="w-full border text-[#3BA576] bg-[#081710] p-2 rounded-lg"
             id="email"
             type="email"
@@ -45,6 +41,7 @@ function submitForm() {
             Message
           </label>
           <textarea
+            name="Message"
             class="w-full border text-[#3BA576] bg-[#081710] p-2 rounded-lg"
             id="message"
             rows="5"
@@ -53,9 +50,8 @@ function submitForm() {
         </div>
         <div class="flex items-center justify-between">
           <button
-          type="submit"
+            type="submit"
             class="border-2 border-[#3BA576] text-[#3BA576] py-2 px-4 rounded-lg hover:bg-[#3BA576] hover:text-black"
-            @click="submitForm"
           >
             Submit
           </button>
