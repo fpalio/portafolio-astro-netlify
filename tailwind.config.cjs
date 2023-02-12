607b15260def191357ec50100b4c24b1a4b11051
Francisco Paliouras <franciscopaliouras@gmail.com>
2023-02-11 21:17:05 -0500

    animated 404

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "bounce-slow": "bounce 4s linear infinite",
+      },
+    },
   },
   plugins: [],
 };
